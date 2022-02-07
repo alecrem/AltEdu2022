c8fe5bfcc7fdbec02bcac41e8729d6e35da9b5b1
Alejandro Cremades <alecrem@users.noreply.github.com>
2022-02-07 23:16:40 +0900

    Characters are now animated

--- 2022-02-07/sketch.js
+++ 2022-02-07/sketch.js
@@ -9,12 +9,11 @@ const stage = [
   [0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 1, 0, 0, 3, 3, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0],
-  [0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 3, 3, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
-var tile_size, offsetX, offsetY;
 const tile_colours = [
   "#c3f6f6", // 0 空色
   "#754702", // 1 茶色
@@ -24,29 +23,87 @@ const tile_colours = [
   "#071380", // 5 青
   "#7bc213", // 6 黄緑
 ];
+var tile_size, offsetX, offsetY;
+const characterPositions = [
+  [10, 2],
+  [10, 2],
+  [10, 2],
+  [9, 3],
+  [8, 4],
+  [9, 4],
+  [10, 4],
+  [10, 4],
+  [10, 5],
+  [10, 6],
+  [10, 7],
+  [10, 8],
+  [10, 9],
+  [9, 10],
+  [8, 11],
+  [7, 12],
+  [6, 13],
+  [6, 14],
+  [7, 15],
+  [8, 0],
+  [9, 1],
+];
+const enemyPositions = [
+  [10, 10],
+  [10, 9],
+  [10, 8],
+  [10, 7],
+  [10, 6],
+  [10, 5],
+  [10, 4],
+  [10, 4],
+  [10, 0],
+  [10, 1],
+  [10, 2],
+  [10, 3],
+  [10, 4],
+  [10, 5],
+  [10, 6],
+  [10, 7],
+  [10, 8],
+  [10, 9],
+  [10, 10],
+  [10, 11],
+  [10, 12],
+];
+var characterX = characterPositions[0][0], characterY = characterPositions[0][1], characterColour = tile_colours[4];
+var enemyX = enemyPositions[0][0], enemyY = enemyPositions[0][1], enemyColour = tile_colours[6];
+var timeLapsed = 0, currentPosition = 0;
+const step = 500;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-    reinit_tile_size();
+  createCanvas(windowWidth, windowHeight);
+  reinit_tile_size();
 }
 
 function draw() {
-	background(0);
-    print_stage(offsetX, offsetY);
+  background(0);
+  if (millis() - timeLapsed >= step) {
+    currentPosition = (currentPosition + 1) % characterPositions.length;
+    timeLapsed = millis();
+  }
+  print_stage(offsetX, offsetY);
+  printCharacter(enemyPositions[currentPosition], enemyColour);
+  printCharacter(characterPositions[currentPosition], characterColour);
 }
 
+// This code by @takawo!
 function mousePressed() {
-	let fs = fullscreen();
-	fullscreen(!fs);
-	if (fs) {
-		cursor();
-	} else {
-		noCursor();
-	}
+  let fs = fullscreen();
+  fullscreen(!fs);
+  if (fs) {
+    cursor();
+  } else {
+    noCursor();
+  }
 }
 
 function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
     reinit_tile_size();
 }
 
@@ -59,10 +116,16 @@ const drawSquare = (i, j, tile='#000000', size=32, offsetX = 0, offsetY = 0) => 
 const print_stage = (offsetX = 0, offsetY = 0) => {
   for (var i = 0; i < stage.length; i++) {
     for (var j = 0; j < stage[0].length; j++) {
-      drawSquare(i, j, tile_colours[stage[i][j]], tile_size, offsetX, offsetY)
+      drawSquare(i, j, tile_colours[stage[i][j]], tile_size, offsetX, offsetY);
     }
   }
 }
+
+const printCharacter = (charPos, charColour) => {
+  fill(charColour);
+  stroke(charColour);
+  drawSquare(charPos[0], charPos[1], charColour, tile_size, offsetX, offsetY);
+};
 
 const reinit_tile_size = () => {
     tile_size = min(windowWidth/stage[0].length, windowHeight/stage.length);
